30b11e5a6372d7848ea821042692c0d3b9db7c82
Ratnesh R. Dubey <ratneshd786@gmail.com>
2019-03-04 13:33:16 +0530

    basic-app is added

---
+++ CoreJava/basic-app/src/com/techlabs/basic/TestOrguments.java
@@ -0,0 +1,22 @@
+package com.techlabs.basic;
+
+public class TestOrguments {
+	public static void main(String[] args) {
+		if (args.length == 0)
+			System.out.println("No Argument has been passed");
+		else {
+
+			/*
+			 * for (int i = 0; i < args.length; i++) {
+			 * 
+			 * System.out.println("hello "+args[i]);
+			 */
+
+			for (String s : args) {
+				System.out.println("hello " + s);
+			}
+
+		}
+
+	}
+}
